b97e1a117da23e50cce27f08372bf5292cf48ee8
jifeng.si <jifeng.si@qunar.com>
2018-01-23 14:44:47 +0800

    ADD:HttpUtil

---
+++ src/main/java/HttpUtil.java
@@ -0,0 +1,123 @@
+import com.google.common.collect.Lists;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * Http工具类
+ * @author sjf0115
+ * @Date Created in 下午2:09 18-1-23
+ */
+public class HttpUtil {
+
+    /**
+     * 配置超时时间
+     */
+    private static RequestConfig requestConfig = RequestConfig.custom()
+            .setConnectTimeout(5000).setConnectionRequestTimeout(5000)
+            .setSocketTimeout(5000).setRedirectsEnabled(true).build();
+    private static CloseableHttpClient httpClient = HttpClients.custom().build();
+
+    /**
+     * Get 请求
+     * @param url
+     * @param paramMap
+     * @return
+     * @throws URISyntaxException
+     * @throws IOException
+     */
+    public static String httpGet(String url, Map<String, String> paramMap) throws URISyntaxException, IOException {
+
+        checkNotNull(url);
+        checkNotNull(paramMap);
+        HttpGet httpGet = new HttpGet(url);
+        httpGet.setConfig(requestConfig);
+        URIBuilder uriBuilder = new URIBuilder(url);
+        if(!paramMap.isEmpty()) {
+            List<NameValuePair> paramsList = paramsConverter(paramMap);
+            uriBuilder.setParameters(paramsList);
+        }
+        httpGet.setURI(uriBuilder.build());
+        CloseableHttpResponse httpResponse = httpClient.execute(httpGet);
+        return toString(httpResponse);
+
+    }
+
+    /**
+     * Post请求
+     * @param url
+     * @param paramMap
+     * @return
+     * @throws URISyntaxException
+     * @throws IOException
+     */
+    public static String httpPost(String url, Map<String, String> paramMap) throws URISyntaxException, IOException {
+
+        checkNotNull(url);
+        checkNotNull(paramMap);
+        HttpPost httpPost = new HttpPost(url);
+        httpPost.setConfig(requestConfig);
+        URIBuilder uriBuilder = new URIBuilder(url);
+        if(!paramMap.isEmpty()) {
+            List<NameValuePair> paramsList = paramsConverter(paramMap);
+            uriBuilder.setParameters(paramsList);
+        }
+        httpPost.setURI(uriBuilder.build());
+        CloseableHttpResponse httpResponse = httpClient.execute(httpPost);
+        return toString(httpResponse);
+
+    }
+
+    /**
+     * 参数转换
+     * @param paramMap
+     * @return
+     */
+    private static List<NameValuePair> paramsConverter(Map<String, String> paramMap){
+
+        checkNotNull(paramMap);
+        List<NameValuePair> paramsList = Lists.newArrayList();
+        Set<Map.Entry<String, String>> entries = paramMap.entrySet();
+        for (Map.Entry<String, String> paramEntry : entries) {
+            paramsList.add(new BasicNameValuePair(paramEntry.getKey(), paramEntry.getValue()));
+        }
+        return paramsList;
+
+    }
+
+    /**
+     * 转换为String
+     * @param httpResponse
+     * @return
+     * @throws IOException
+     */
+    private static String toString(CloseableHttpResponse httpResponse) throws IOException {
+
+        checkNotNull(httpResponse);
+        int statusCode = httpResponse.getStatusLine().getStatusCode();
+        int errorCode = 200;
+        if (statusCode != errorCode) {
+            throw new RuntimeException("http 请求失败");
+        }
+        String json = EntityUtils.toString(httpResponse.getEntity());
+        return json;
+
+    }
+
+}
---
+++ src/test/java/HttpUtilTest.java
@@ -0,0 +1,43 @@
+import com.google.common.collect.Maps;
+import org.junit.Test;
+
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author sjf0115
+ * @Date Created in 下午2:28 18-1-23
+ */
+public class HttpUtilTest {
+    @Test
+    public void httpGet() throws Exception {
+        String url = "https://sp0.baidu.com/8aQDcjqpAAV3otqbppnN2DJv/api.php";
+        Map<String, String> paramsMap = Maps.newHashMap();
+        paramsMap.put("query", "218.59.255.255");
+        paramsMap.put("resource_id","6006");
+        paramsMap.put("t","1516689086692");
+        paramsMap.put("ie","utf8");
+        paramsMap.put("oe","gbk");
+        paramsMap.put("cb","op_aladdin_callback");
+        paramsMap.put("format","json");
+        paramsMap.put("tn", "baidu");
+        paramsMap.put("cb", "jQuery110205798053672419516_1516689071181&_=1516689071186");
+
+        String json = HttpUtil.httpGet(url, paramsMap);
+        System.out.println(json);
+
+    }
+
+    @Test
+    public void httpPost() throws Exception {
+
+        String url = "http://ip.taobao.com/service/getIpInfo2.php";
+        Map<String, String> paramsMap = Maps.newHashMap();
+        paramsMap.put("ip", "218.59.255.255");
+        String json = HttpUtil.httpPost(url, paramsMap);
+        System.out.println(json);
+
+    }
+
+}
